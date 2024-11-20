cfe13966681088f070974340d627f24884730bb7
Henrique Souza <henrique@Henriques-iMac-Pro.local>
2024-11-19 22:13:26 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import { Link } from "expo-router";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Po</Text>
+      <Link href={{ pathname: "/screens/auth/login" }}>Login</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
